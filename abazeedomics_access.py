a8ea4ce6befd35584172add4dd08d55619d87e1d
Semihcan Doken <sdoken@gmail.com>
2018-10-10 17:45:45 -0400

    added startswith conditional argument to listdir_hidden

--- abazeedomics_access.py
+++ abazeedomics_access.py
@@ -1,8 +1,15 @@
 import os
-def listdir_nohidden(path_to_folder):
+
+def listdir_nohidden(path_to_folder, startswith=False):
     list_of_files = []
-    for f in os.listdir(path_to_folder):
-        if f.startswith('P'):
-            list_of_files.append(f)
-    list_of_files.sort()
+    if startswith:
+        for f in os.listdir(path_to_folder):
+            if f.startswith(startswith):
+                list_of_files.append(f)
+        list_of_files.sort()
+    else :
+        for f in os.listdir(path_to_folder):
+            if not f.startswith('.'):
+                list_of_files.append(f)
     return list_of_files
+
