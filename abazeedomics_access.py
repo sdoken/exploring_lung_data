55317feeeea16743c2bc4bd2f30e7bbf8c0c115a
Semihcan Doken <sdoken@gmail.com>
2018-10-11 11:41:01 -0400

    shorter with list_comprehensions

--- abazeedomics_access.py
+++ abazeedomics_access.py
@@ -1,5 +1,15 @@
 import os
 
+def listdir_nohidden_two(path_to_folder, startswith=False):
+    if startswith:
+        list_of_files = [f for f in os.listdir(path_to_folder) if f.startswith(startswith) ]
+    else :
+        list_of_files = [f for f in os.listdir(path_to_folder) if not(f.startswith('.') or 
+                                                                    f.startswith('Desk'))]
+    list_of_files.sort()
+    return list_of_files
+
+'''
 def listdir_nohidden(path_to_folder, startswith=False):
     list_of_files = []
     if startswith:
@@ -7,9 +17,11 @@ def listdir_nohidden(path_to_folder, startswith=False):
             if f.startswith(startswith):
                 list_of_files.append(f)
         list_of_files.sort()
+        return list_of_files
     else :
         for f in os.listdir(path_to_folder):
-            if not f.startswith('.'):
+            if not (f.startswith('.') or f.startswith('Desk') ):
                 list_of_files.append(f)
-    return list_of_files
-
+        list_of_files.sort()
+        return list_of_files
+'''
